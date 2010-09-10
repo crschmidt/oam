bb4d9479e03cbd97665aad4f450d8bdff6188415
Schuyler Erle <schuyler@simplegeo.com>
2010-09-10 15:55:09 +0200

    Add MD5 hashing.

--- catalog-client/oam-post.py
+++ catalog-client/oam-post.py
@@ -2,6 +2,10 @@
 
 import sys, os, optparse, urllib2
 try:
+    from hashlib import md5
+except ImportError:
+    import md5
+try:
     from osgeo import gdal, gdalconst
 except ImportError:
     import gdal, gdalconst
@@ -16,6 +20,7 @@ def parse_options():
     parser.add_option("-P", "--password", dest="pass", help="OAM password")
     parser.add_option("-s", "--service", dest="service",
         help="OAM service base URL", default="http://adhoc.osgeo.osuosl.org:8000/")
+    #parser.add_option("-d", "--debug", dest="debug", action="store_true", default=False, help="Debug mode (dump HTTP errors)")
     parser.add_option("-t", "--test", dest="test", action="store_true", default=False, help="Test mode (don't post to server)")
     parser.add_option("-l", "--layer", dest="layer", type="int", help="Layer ID")
     parser.add_option("-u", "--url", dest="url", help="Image URL")
@@ -31,7 +36,18 @@ def parse_options():
             "URL must end with a / if multiple files are being stored")
     return opts
 
+def compute_md5(filename,blocksize=(1<<20)):
+    filesize = os.path.getsize(filename)
+    f = file(filename, "rb")
+    h = md5.md5()
+    for i in range(0, filesize, blocksize):
+        h.update(f.read(blocksize)) 
+        print >>sys.stderr, "\rComputing MD5... %d%%" % (float(i)/filesize*100),
+    print >>sys.stderr, "\rComputing MD5... done."
+    return h.hexdigest()
+
 def extract_metadata(filename):
+    print >>sys.stderr, "Reading metadata from %s." % filename
     dataset = gdal.Open(filename, gdalconst.GA_ReadOnly)
     if dataset is None:
         raise Exception("Cannot open %s" % filename)
@@ -49,7 +65,8 @@ def extract_metadata(filename):
         "height": dataset.RasterYSize,
         "crs": dataset.GetProjection(),
         "file_size": os.path.getsize(filename),
-        "bbox": bbox
+        "bbox": bbox,
+        "hash": compute_md5(filename)
     }
 
 def generate_description(filename, opts):
@@ -63,13 +80,16 @@ def generate_description(filename, opts):
 def post_description(filename, opts):
     record = generate_description(filename, opts)
     content = json.dumps(record)
+    print >>sys.stderr, "Uploading description...",
     req = urllib2.Request(opts.service + "image/")
     try:
-        response = urllib2.urlopen(req)
+        response = urllib2.urlopen(req, content)
     except IOError, e:
+        print >>sys.stderr, "error."
         raise
-    result = f.read()
-    return json.reads(result)
+    result = response.read()
+    print >>sys.stderr, "done."
+    return json.loads(result)
 
 if __name__ == "__main__":
     import pprint
@@ -79,4 +99,4 @@ if __name__ == "__main__":
             record = generate_description(filename, opts)
         else:
             record = post_description(filename, opts)
-    pprint.pprint(record)
+        print json.dumps(record)

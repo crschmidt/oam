933e7fbe5e075866d7175005344bcca906488248
Christopher Schmidt <crschmidt@crschmidt.net>
2010-10-05 11:13:47 -0400

    Add support to create license objects directly, rather than only through images.

--- catalog-server/oamcatalog/main/urls.py
+++ catalog-server/oamcatalog/main/urls.py
@@ -4,6 +4,8 @@ urlpatterns = patterns('main.views',
     # Example:
     (r'^api/layer/$', 'layer'),
     (r'^api/layer/(?P<id>[0-9]+)/$', 'layer'),
+    (r'^api/license/$', 'license'),
+    (r'^api/license/(?P<id>[0-9]+)/$', 'license'),
     (r'^api/image/$', 'image'),
     (r'^api/image/(?P<id>[0-9]+)/$', 'image'),
 )
--- catalog-server/oamcatalog/main/views.py
+++ catalog-server/oamcatalog/main/views.py
@@ -22,6 +22,23 @@ def layer(request, id=None):
         }    
         return json_response(request, data)
             
+@jsonexception
+def license(request, id=None):
+    if id == None and request.method == "POST":
+        data = simplejson.loads(request.raw_post_data)
+        l = License()
+        l.from_json(data)
+        return json_response(request, l)
+    elif id != None:
+        l = License.objects.get(pk=id)
+        return json_response(request, l)
+    else:
+        licenses = License.objects.all()
+        data = {'licenses': [
+            l.to_json() for l in licenses 
+            ]
+        }   
+        return json_response(request, data)
 
 @jsonexception
 def image(request, id=None):

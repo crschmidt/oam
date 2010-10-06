79c111e8ee258d3312f5ddc341b5d5ab2309a838
Christopher Schmidt <crschmidt@osgeo.org>
2010-10-06 03:46:45 +0000

    Change the models to not require the non-required fields, and add a (crappy) bbox search to the /api/image/ API.

--- catalog-server/oamcatalog/main/views.py
+++ catalog-server/oamcatalog/main/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 from django.http import HttpResponse
 import simplejson
-from main.models import Layer, Image, User
+from main.models import Layer, Image, User, License
 from main.helpers import *
 
 @jsonexception
@@ -46,14 +46,33 @@ def image(request, id=None):
         data = simplejson.loads(request.raw_post_data)
         i = Image()
         i.from_json(data)
+        i.save()
         return json_response(request, i)
     elif id != None:
         i = Image.objects.get(pk=id)
+        if request.method == "POST":
+            i.from_json()
+            i.save()
         return json_response(request, i)
     else:
         images = Image.objects.all()
+        limited_images = images
+        if 'bbox' in request.GET:
+            limited_images = []
+            left, bottom, right, top = map(float, request.GET['bbox'].split(","))
+            for image in images:
+                ileft, ibottom, iright, itop = map(float, image.bbox.split(","))
+                inbottom = (((ibottom >= bottom) and (ibottom <= top)) or ((bottom >= ibottom) and (bottom <= itop)))
+                intop = (((itop >= bottom) and (itop <= top)) or ((top > ibottom) and (top < itop)))
+                inleft = (((ileft >= left) and (ileft <= right)) or ((left >= ileft) and (left <= iright)))
+                inright = (((iright >= left) and (iright <= right)) or ((right >= iright) and (right <= iright)))
+                intersects = ((inbottom or intop) and (inleft or inright))
+                if intersects: 
+                    limited_images.append(image)
+                   
+                
         data = {'images': [
-            i.to_json() for i in images 
+            i.to_json() for i in limited_images
             ]
         }   
         return json_response(request, data)

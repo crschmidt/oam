2a7de1c1881d941d9b362a620a118c84cd6dc6cb
Schuyler Erle <schuyler@simplegeo.com>
2010-10-16 13:52:11 -0700

    Hooray! gdalmask works!

--- storage/gdalmask.py
+++ storage/gdalmask.py
@@ -6,13 +6,12 @@ try:
 except ImportError:
     import gdal
 
-import sys
+import sys, array
 
 gdal.SetConfigOption( 'GDAL_TIFF_INTERNAL_MASK', 'YES' )
 
-source = gdal.Open(sys.argv[1])
+source = gdal.Open(sys.argv[1], gdal.GA_ReadOnly)
 bands = [source.GetRasterBand(i+1) for i in range(source.RasterCount)]
-block_width, block_height = bands[0].GetBlockSize()
 
 # check that bands 1-3 are RGB
 assert [band.GetColorInterpretation() for band in bands] == [gdal.GCI_RedBand, gdal.GCI_GreenBand, gdal.GCI_BlueBand]
@@ -21,11 +20,12 @@ target = gdal.GetDriverByName('GTiff').Create( sys.argv[2],
                                           source.RasterXSize,
                                           source.RasterYSize,
                                           3, gdal.GDT_Byte,
-                                          [ #'PHOTOMETRIC=YCBCR',
+                                          [ 'PHOTOMETRIC=YCBCR',
                                             'COMPRESS=JPEG',
                                             'TILED=YES' ] ) 
 
 target_bands = [target.GetRasterBand(i+1) for i in range(3)]
+block_width, block_height = target_bands[0].GetBlockSize()
 
 # copy georef
 target.SetProjection(source.GetProjection())
@@ -36,41 +36,49 @@ nodata = [band.GetNoDataValue() for band in bands]
 use_alpha = (len(bands) > 4)
 use_nodata = (not use_alpha and len([n for n in nodata if n is not None]) == 3)
 
+print >>sys.stderr, "Generating mask from",
 if use_alpha:
-    print >>sys.stderr, "Using alpha band to generate mask."
+    print >>sys.stderr, "alpha band."
 else:
     if use_nodata:
-        print >>sys.stderr, "Using NODATA values to generate mask."
+        print >>sys.stderr, "NODATA values."
         nodata = [chr(int(n)) for n in nodata]
     else:
-        print >>sys.stderr, "Using black to generate mask."
+        print >>sys.stderr, "black pixels."
         nodata = ["\0", "\0", "\0"]
         use_nodata = True
 
 target.CreateMaskBand(gdal.GMF_PER_DATASET)
 mask_band = target_bands[0].GetMaskBand()
 
-for col in range(0, source.RasterXSize, block_width):
-    for row in range(0, source.RasterYSize, block_height):
-        data = [band.ReadRaster(col, row, block_width, block_height) for band in bands[:3]]
-        mask = '' # ['\1'] * len(data[0])
+total_blocks = (target.RasterXSize * target.RasterYSize) / float(block_width * block_height)
+blocks_written = 0
+sys.stderr.write("0...")
+
+for col in range(0, target.RasterXSize, block_width):
+    for row in range(0, target.RasterYSize, block_height):
+        width = min(target.RasterXSize - col, block_width)
+        height = min(target.RasterYSize - row, block_height)
+        data = [band.ReadRaster(col, row, width, height) for band in bands[:3]]
+        mask = array.array("B", (1 for x in range(len(data[0]))))
         alpha = None
         if use_alpha:
-            alpha = band[3].ReadRaster(col, row, block_width, block_height)
+            alpha = band[3].ReadRaster(col, row, width, height)
         for byte in range(len(data[0])):
-            value = '\1'
             if use_alpha:
                 if not alpha[byte]:
-                    value ='\0' #mask[byte] = '\0'
+                    mask[byte] = 0
             else:
                 if data[0][byte] == nodata[0] and data[1][byte] == nodata[1] and data[2][byte] == nodata[2]:
-                    value = '\0' #mask[byte] = '\0'
-            mask += value
+                    mask[byte] = 0
         for n, block in enumerate(data):
             target_bands[n].WriteRaster(col, row, block_width, block_height, block)
-        #mask = "".join(mask)
-        mask_band.WriteRaster(col, row, block_width, block_height, mask)
-        sys.stderr.write(".")
+        mask_band.WriteRaster(col, row, block_width, block_height, mask.tostring())
+        
+        if int(blocks_written / total_blocks * 10) != int((blocks_written + 1) / total_blocks * 10):
+            count = int((blocks_written + 1) / total_blocks * 10)
+            sys.stderr.write("done." if count == 10 else "%d0..." % count)
+        blocks_written += 1
 
 sys.stderr.write("\n")
 mask_band = None
